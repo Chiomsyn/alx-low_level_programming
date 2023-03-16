832969588fc7d32482248dce83d2d32bd146b00a
Chioma <mirianangelk@gmail.com>
2023-03-16 15:04:30 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - print given strings using puts function
  *
  * Return: 0
- */
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	return (0);
 }
