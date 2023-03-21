910315b9262538ad2d4863ec4ec0ee17e2e1cdab
Chioma <mirianangelk@gmail.com>
2023-03-21 04:33:13 -0700

    program that prints putchar on a page

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Printsnthe character _putchar
+ *
+ * Return: On success 0
+ *
+ */
+int main(void)
+{
+	char val[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int len = strlen(val);
+	int i = 0;
+
+	while (i < len)
+	{
+		putchar(val[i]);
+		i++;
+	}
+	return (0);
+}
