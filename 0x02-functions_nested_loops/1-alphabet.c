7deebb0afcb0638ce0522f8ad10026f6aa3278ae
Chioma <mirianangelk@gmail.com>
2023-03-21 06:22:34 -0700

    worked on task 1: print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  Prints the alphabet a-z
+ *
+ * Return: Return void
+ *
+ */
+void print_alphabet(void)
+{
+	char data = 'a';
+
+	while (data <= 'z')
+	{
+		_putchar(data);
+		data++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-int _putchar(char x);
+int _putchar(char);
+void print_alphabet(void);
 
 #endif
