fb8beefd8e3e0fbf29674c45c5a077a0aad2655b
Chioma <mirianangelk@gmail.com>
2023-03-21 18:00:14 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ *
+ */
+void times_table(void)
+{
+	int w, x, mul;
+
+	for (w = 0; w < 10; w++)
+	{
+		_putchar(48);
+		for (x = 0; x < 10; x++)
+		{
+			mul = x + w;
+			_putchar(44);
+			_putchar(32);
+			if (mul < 10)
+			{
+				_putchar(32);
+				_putchar(mul + 48);
+			}
+			else
+			{
+				_putchar((mul / 10) + 48);
+				_putchar((mul % 10) + 48);
+			}
+		}
+		_putchar(10);
+	}
+}
