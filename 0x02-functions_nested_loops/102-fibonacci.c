edd74e29bbd63d223bd0be10841d762e14881bb3
Chioma <mirianangelk@gmail.com>
2023-03-21 19:45:28 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the add of the Fibonacci numbers
+ *
+ * Return: zero.
+ */
+int main(void)
+{
+	int i;
+	long sum, prev, nxt;
+
+	sum = prev = nxt = 1;
+	for (i = 1; i < 50; i++)
+	{
+		if (i > 1)
+		{
+			if (sum != 1)
+			{
+				prev = nxt;
+				nxt = sum;
+			}
+		}
+		else if (i == 1)
+		{
+			nxt = 1;
+			printf("%d, ", 1);
+		}
+		else
+		{
+
+		}
+		sum = prev + nxt;
+		printf("%ld, ", sum);
+	}
+	printf("\n");
+	return (0);
+}
