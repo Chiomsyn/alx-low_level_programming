16979037ad85fc1ff7d2e60e171a51d2841efe4e
Chioma <mirianangelk@gmail.com>
2023-03-22 23:43:24 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: The character to check
+ *
+ * Return: Returns 1 if true and 0 if false
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
