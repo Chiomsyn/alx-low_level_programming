4ea34159ce093080a7033cd7fd4845894f4729ad
Chioma <mirianangelk@gmail.com>
2023-03-23 00:02:56 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - checks for a digit (0 through 9)
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: Returns 1 if true and 0 if false
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
