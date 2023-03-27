ae3b42e56872cbbbcf2e901c3e37839036987992
Chioma <mirianangelk@gmail.com>
2023-03-27 06:40:13 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts_half(char *str)
+{
+	int count = 0, i;
+
+	while (*(s + count) != '\0')
+		count++;
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
