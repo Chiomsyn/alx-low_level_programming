3514006b5c9154fc38ebc5942ca7b105257cef4b
Chioma <mirianangelk@gmail.com>
2023-03-27 06:01:26 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts2(char *str)
+{
+	int count;
+
+	for (count = 0; str[count] != '\0'; count = count + 2)
+		_putchar(str[count]);
+	_putchar('\n');
