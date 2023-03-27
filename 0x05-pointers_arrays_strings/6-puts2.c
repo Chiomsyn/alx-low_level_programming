c2fa6877c737df1ee5de12d7999b7fe5f5115ee6
Chioma <mirianangelk@gmail.com>
2023-03-27 07:23:52 -0700

    resolved 6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,11 @@ void puts2(char *str)
 {
 	int count;
 
-	for (count = 0; str[count] != '\0'; count = count + 2)
-		_putchar(str[count]);
-	_putchar('\n');
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		if (count  % 2 == 0)
+		{
+			_putchar(str[count]);
+		}
+		_putchar('\n');
 }
