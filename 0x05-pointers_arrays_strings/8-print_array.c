c39c23ad636114ff7cc2eceb45704d705c6ecd1b
Chioma <mirianangelk@gmail.com>
2023-03-27 08:05:54 -0700

    resolved 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,8 @@ void print_array(int *a, int n)
 	while (i < n)
 	{
 		printf("%d", *(a + i));
-
-		if (i == (n - 1))
-			break;
-		_putchar(44);
-		_putchar(32);
+		if (i != (n - 1))
+			printf(", ");
 		i++;
 	}
 	_putchar(10);
