e26eec6ddeee631e23477fa825dda72b4f8d1226
Chioma <mirianangelk@gmail.com>
2023-04-04 18:56:14 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: initial segment.
+ * @accept: accepted bytes.
+ * Return: the number of accepted bytes.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, check;
+
+	i = j = 0;
+	while (*(s + i) != '\0')
+	{
+		check = 1;
+		for (; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				check = 0;
+				break;
+			}
+		}
+		if (check == 1)
+			break;
+	}
+	return (i);
+}
