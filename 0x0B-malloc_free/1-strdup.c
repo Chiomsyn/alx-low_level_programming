1658fba3a4e18591a9016043b2e85457050bb8fa
Chioma <mirianangelk@gmail.com>
2023-04-16 17:26:53 -0700

    hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
-	tmpstr =(char *) malloc(sizeof(char) * (i + 1));
+	tmpstr =(char *)malloc(sizeof(char) * (i + 1));
 
 	if (tmpstr == NULL)
 		return (NULL);
