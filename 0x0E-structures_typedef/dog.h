cac1797dcab7a1199b26a48a56a354604c94f4a7
Chioma <mirianangelk@gmail.com>
2023-04-18 04:08:28 -0700

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * struct dog - struct that stores information of a dog
  * @name: name of the dog
  * @age: age of the dog
- * @owner of the dog
+ * @owner: owner of the dog
  *
  * Description: struct with name "dog" that saves its name, its age and its owner's name
  */
@@ -18,4 +18,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
